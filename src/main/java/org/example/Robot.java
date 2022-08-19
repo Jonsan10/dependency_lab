1519c3f7b440977a293be461622868d7ec1e22de
jonathan sanchez <sanchezjon480@gmail.com>
2022-08-19 11:23:02 -0700

    moved files to github repo

---
+++ src/main/java/org/example/Robot.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Robot {
+
+    //Instance variables (HAS A)
+    public String name;
+    public String weaponName;
+    public int weaponPower;
+
+    //Constructor
+    public Robot() {
+        this.name = "Larry";
+        this.weaponName = "Sword";
+        this.weaponPower = 50;
+    }
+    //Methods (CAN DO)
+    public void Attack(){
+        System.out.println(this.name + "attacks with their" + this.weaponPower);
+    }
+}
